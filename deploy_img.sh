c402f5993353f61222f63edb1f8be958284ec65a
Kail <kfkkail@gmail.com>
2014-05-19 21:43:18 -0400

    fixed img deploy

--- deploy_img.sh
+++ deploy_img.sh
@@ -1,1 +1,1 @@
-ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/htpc/Documents/git/andreaandkail/lib/
+ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/htpc/Documents/git/andreaandkail/img/
