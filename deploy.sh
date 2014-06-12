169ced4ead4008589718cc51e03d0fc2cfd7f314
Kail <kfkkail@gmail.com>
2014-06-12 12:07:22 -0400

    add proposal and aboutus to deploy

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@ ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /ho
 ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/css/
 ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/index.html
 ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/media.html
+ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/proposal.html
+ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/aboutus.html
