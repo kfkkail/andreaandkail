76225c4aaa26f05bdfd7c28d01e20c8851408adb
Kail Keusch <kfkkail@gmail.com>
2016-11-08 11:18:47 -0500

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/js/
-ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/css/
-ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/index.html
-ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/missing.html
